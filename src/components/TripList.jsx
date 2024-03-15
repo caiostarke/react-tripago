0943254f2599f070a37b70f23a2c6cb0f30ac7d3
crowlke <caiostarke@hotmail.com>
2024-03-15 10:21:36 -0300

    add useEffect url dependency to query location

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -5,13 +5,14 @@ import './TripList.css'
 
 export default function TripList() {
     const [trips, setTrips] = useState([])
+    const [url, setUrl] = useState('http://localhost:3000/trips')
 
     useEffect(() => {
-        fetch('http://localhost:3000/trips')
+        fetch(url)
         .then(res => res.json())
         .then(data => setTrips(data))
         .catch(e => console.log(e))    
-    }, [])
+    }, [ url ])
 
   return (
     <div className='trip-list'>
@@ -19,6 +20,15 @@ export default function TripList() {
       <ul>
         {trips.map( trip => <li key={trip.id}>{trip.title} <p> {trip.price}</p> </li>   )}
       </ul>
+      <div className="filters">
+        <button onClick={() => setUrl('http://localhost:3000/trips?location=Europe')}>
+            European Trips
+        </button>
+        <button onClick={() => setUrl('http://localhost:3000/trips')}>
+            All Trips
+        </button>
+      </div>
     </div>
   )
 }
+ 
